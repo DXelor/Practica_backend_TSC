1ef1158fdfbe3ffefbecfe7a4a67446d8c209e73
DXelor <dzaliaspro@gmail.com>
2021-01-08 19:34:02 -0500

    Configurando el CORS

--- dist/index.js
+++ dist/index.js
@@ -6,9 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./classes/server"));
 const router_1 = __importDefault(require("./routes/router"));
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const server = new server_1.default();
 //body parser
 server.app.use(express_1.default.urlencoded({ extended: true }));
 server.app.use(express_1.default.json());
+//Configurando el CORS
+server.app.use(cors_1.default({ origin: true, credentials: true }));
+//rutas de servicios
 server.app.use('/', router_1.default);
 server.start(() => console.log(`server iniciado en el puerto: ${server.port}`));
--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import Server from './classes/server';
 import router from './routes/router';
 import express from 'express';
+import cors from 'cors';
 
 const server = new Server();
 
@@ -9,6 +10,11 @@ const server = new Server();
 server.app.use(express.urlencoded({extended:true}));
 server.app.use(express.json());
 
+//Configurando el CORS
+server.app.use(cors({origin:true, credentials:true}));
+
+
+//rutas de servicios
 server.app.use('/', router);
 
 server.start( () => console.log(`server iniciado en el puerto: ${ server.port }`))
